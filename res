d6a8b49a8e8db9b6c7acedafdc7833249c027973
Tau <tauoverpi@yandex.com>
2020-06-20 21:02:34 +0200

    Initial work

---
+++ res
@@ -0,0 +1,34 @@
+#actor
+Actor Model of Computation: Scalable Robust Information Systems
+The Actor Model is a mathematical theory that treats “Actors” as the
+universal primitives of digital computation.  The model has been used
+both as a framework for a theoretical understanding of concurrency, and as
+the theoretical basis for several practical implementations of concurrent
+systems. The advent of massive concurrency through client-cloud computing
+and many-core computer architectures has galvanized interest in the Actor Model
+^
+@https://arxiv.org/pdf/1008.1459.pdf
+#actor
+Formalizing common sense reasoning for scalable inconsistency-robust information integration using Direct LogicTM Reasoning and the Actor Model
+People use common sense in their interactions with large information systems.
+This common sense needs to be formalized so that it can be used by computer
+systems. Unfortunately, previous formalizations have been inadequate. For
+example, classical logic is not safe for use with pervasively inconsistent
+information. The goal is to develop a standard foundation for reasoning in
+large-scale Internet applications (including sense making for natural
+language)
+^
+@https://arxiv.org/pdf/0812.4852.pdf
+#bigraphs
+Bigraphical reactive systems: basic theory
+A notion of bigraph is proposed as the basis for a model of mobile
+interaction. A bigraph consists of two independent structures: a topograph
+representing locality and a monograph representing connectivity. Bigraphs
+are equipped with reaction rules to form bigraphical reactive systems (BRSs),
+which include versions of the -calculus and the ambient calculus. Bigraphs are
+shown to be a special case of a more abstract notion, wide reactive systems
+(WRSs), not assuming any particular graphical or other structure but equipped
+with a notion of width, which expresses that agents, contexts and reactions
+may all be widely distributed entities
+^
+@https://www.cl.cam.ac.uk/techreports/UCAM-CL-TR-523.pdf
---
+++ src/main.zig
@@ -0,0 +1,146 @@
+const std = @import("std");
+
+const Token = union(enum) {
+    Tag: usize,
+    Title: usize,
+    Description: usize,
+    Hash: usize,
+    Link: usize,
+
+    pub fn slice(self: Token, index: usize, text: []const u8) usize {
+        switch (self) {
+            .Tag, .Title, .Description, .Hash, .Link => |x| return text[0..],
+        }
+    }
+};
+
+const StreamingParser = struct {
+    count: usize,
+    state: State,
+
+    const State = enum {
+        LinkEnd,
+        Tag,
+        Title,
+        Desc,
+        DescCont,
+        Hash,
+        HashEnd,
+        Link,
+    };
+
+    pub fn init() StreamingParser {
+        var p: StreamingParser = undefined;
+        p.reset();
+        return p;
+    }
+
+    pub fn reset(self: *StreamingParser) void {
+        self.count = 0;
+        self.state = .LinkEnd;
+    }
+
+    pub fn feed(self: *StreamingParser, c: u8) !?Token {
+        self.count += 1;
+        switch (self.state) {
+            .LinkEnd => switch (c) {
+                '#' => {
+                    self.count = 0;
+                    self.state = .Tag;
+                },
+                else => return error.InvalidSection,
+            },
+            .Tag => switch (c) {
+                '\n' => {
+                    const token = Token{ .Tag = self.count };
+                    self.count = 0;
+                    self.state = .Title;
+                    return token;
+                },
+                else => {},
+            },
+            .Title => switch (c) {
+                '\n' => {
+                    const token = Token{ .Title = self.count };
+                    self.count = 0;
+                    self.state = .Desc;
+                    return token;
+                },
+                else => {},
+            },
+            .Desc => switch (c) {
+                '\n' => self.state = .DescCont,
+                else => {},
+            },
+            .DescCont => switch (c) {
+                '^' => {
+                    const token = Token{ .Description = self.count };
+                    self.count = 0;
+                    self.state = .Hash;
+                    return token;
+                },
+                '@' => return error.InvalidSection,
+                '#' => return error.InvalidSection,
+                else => self.state = .Desc,
+            },
+            .Hash => switch (c) {
+                '\n' => {
+                    self.state = .HashEnd;
+                    return Token{ .Hash = self.count };
+                },
+                else => {},
+            },
+            .HashEnd => switch (c) {
+                '@' => {
+                    self.count = 0;
+                    self.state = .Link;
+                },
+                else => return error.InvalidSection,
+            },
+            .Link => switch (c) {
+                '\n' => {
+                    self.state = .LinkEnd;
+                    return Token{ .Link = self.count };
+                },
+                else => {},
+            },
+        }
+        return null;
+    }
+};
+
+test "" {
+    const resources =
+        \\#thing
+        \\stuff
+        \\described
+        \\^hashed
+        \\@linked
+        \\#morething
+        \\#withtags
+        \\and a tile with spaces!
+        \\then a very short description
+        \\which spans more than one line
+        \\as it must be able to handle it
+        \\^that unused hash
+        \\@oh noes, a link
+        \\
+    ;
+
+    var p = StreamingParser.init();
+
+    for (resources) |byte| {
+        if (try p.feed(byte)) |item| {
+            _ = item.slice(9, resources[0..]);
+            std.debug.warn("{}\n", .{item});
+        }
+    }
+}
+
+const Resource = struct {
+    topics: TopicIterator,
+};
+
+pub fn main() anyerror!void {
+    std.debug.warn("All your codebase are belong to us.\n", .{});
+}
